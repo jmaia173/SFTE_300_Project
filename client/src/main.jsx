a03786c6f9a322b42f6e2a27318265f1c1a74ab5
jmaia173 <jpmascarenhasmaia@gmail.com>
2025-10-24 22:35:11 -0700

    update

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,9 +3,24 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { ClerkProvider } from '@clerk/clerk-react'
+import { AppContextProvider } from './context/AppContext.jsx'
+
+// Import your Clerk publishable key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key')
+}
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <StrictMode>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl='/'>
+      <BrowserRouter>
+        <AppContextProvider>
+          <App />
+        </AppContextProvider>
+      </BrowserRouter>
+    </ClerkProvider>
+  </StrictMode>
 )
