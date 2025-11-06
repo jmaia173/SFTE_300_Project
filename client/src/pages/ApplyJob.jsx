2b0267d73616db9d27b5689a006a626af69dd147
jmaia173 <jpmascarenhasmaia@gmail.com>
2025-11-06 08:24:00 -0800

    update

--- client/src/pages/ApplyJob.jsx
+++ client/src/pages/ApplyJob.jsx
@@ -1,8 +1,57 @@
-import React from "react";  
+import React, { useContext, useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { AppContext } from "../context/AppContext";
+import Loading from "../components/Loading";
+import Navbar from "../components/Navbar";
+import { assets } from "../assets/assets";
 
 const ApplyJob = () => {
-  return(
-    <div>ApplyJob</div>
+
+  const {id} = useParams()
+
+  const [JobData, setJobData] = useState(null)
+
+  const {jobs} = useContext(AppContext)
+
+    const fetchJob = async () => {
+      const data = jobs.filter(job => String(job._id) === String(id));
+      if (data.length !== 0){
+        setJobData(data[0])
+        console.log(data[0])
+      }
+    }
+  
+    useEffect (() => {
+      if (jobs.length > 0){
+        fetchJob()
+      }
+    
+    },[id,jobs])
+
+  return JobData ? (
+    <>
+      <Navbar />
+      <div>
+        <div>
+            <div>
+              <div>
+                <img src={assets.company_icon} alt=""/>
+                <div>
+                  <h1>{JobData.title}</h1>
+                  <div>
+                    <span>
+                      <img src={assets.suitcase_icon} alt=""/>
+                      {JobData}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
+      </div>
+    </>
+  ) :(
+    <Loading />
   )
 }
 
