0c1552082b15450b4016d91efc801e16bd660d9f
jmaia173 <jpmascarenhasmaia@gmail.com>
2025-11-24 18:12:18 -0800

    q

--- client/src/pages/ManageJobs.jsx
+++ client/src/pages/ManageJobs.jsx
@@ -7,64 +7,69 @@ import { useContext } from "react";
 import { AppContext } from "../context/AppContext";
 import axios from "axios";
 import { useEffect } from "react";
+import Loading from "../components/Loading";
 
-const ManageJobs  = () => {
+const ManageJobs = () => {
 
-const navigate = useNavigate()
+    const navigate = useNavigate()
 
-const [jobs, setJobs] = useState([])
+    const [jobs, setJobs] = useState(false)
 
-const {backendUrl, companyToken} = useContext(AppContext)
+    const { backendUrl, companyToken } = useContext(AppContext)
 
-// function to fetch company Job Applications data
-const fetchCompanyData = async () => {
+    // function to fetch company Job Applications data
+    const fetchCompanyData = async () => {
 
-    try {
-        
-        const {data} = await axios.get(backendUrl+ "/api/company/list-jobs",
-            {headers: {token: companyToken}}
-        )
-        if (data.success) {
-            setJobs(data.jobsData.reverse()) 
-            console.log(data.jobsData);
-            
-        } else {
-            toast.error(data.message)
+        try {
+
+            const { data } = await axios.get(backendUrl + "/api/company/list-jobs",
+                { headers: { token: companyToken } }
+            )
+            if (data.success) {
+                setJobs(data.jobsData.reverse())
+                console.log(data.jobsData);
+
+            } else {
+                toast.error(data.message)
+            }
+
+        } catch (error) {
+            toast.error(error.message)
         }
 
-    } catch (error) {
-        toast.error(error.message)
     }
 
-}
+    // function to change job visibility
+    const changeJobVisibility = async (id) => {
+        try {
 
-// function to change job visibility
-const changeJobVisibility = async (id) => {
-    try {
-        
-        const {data} = await axios.post(backendUrl + "/api/company/change-visibility", 
-        {id},
-        {headers: {token: companyToken}}
-        )
-        if (data.success) {
-            toast.success(data.message)
-            fetchCompanyJobs()
-        } else {
-            toast.error(data.message)
+            const { data } = await axios.post(backendUrl + "/api/company/change-visibility",
+                { id },
+                { headers: { token: companyToken } }
+            )
+            if (data.success) {
+                toast.success(data.message)
+                fetchCompanyJobs()
+            } else {
+                toast.error(data.message)
+            }
+
+        } catch (error) {
+            toast.error(error.message)
         }
-
-    } catch (error) {
-        toast.error(error.message)
     }
-}
 
     useEffect(() => {
-        if(companyToken) {
+        if (companyToken) {
             fetchCompanyData()
         }
     }, [companyToken])
 
-    return (
+    return jobs ? jobs.lenght === 0 ? (
+        <div className="flex items-center justify-center h-[70vh]">
+            <p className="text-xl sm:text-2xl">No Jobs Avaiable or posted</p>
+        </div>
+    ) : (
         <div className="container p-4 max-w-5xl">
             <div className="overflow-x-auto">
                 <table className="min-w-full bg-white border border-gray-200 max-sm:text-sm">
@@ -79,15 +84,15 @@ const changeJobVisibility = async (id) => {
                         </tr>
                     </thead>
                     <tbody>
-                        {jobs.map((job, index)=> (
+                        {jobs.map((job, index) => (
                             <tr key={index} className="text-gray-700">
-                                <td className="py-2 px-4 border-b max-sm:hidden">{index+1}</td>
+                                <td className="py-2 px-4 border-b max-sm:hidden">{index + 1}</td>
                                 <td className="py-2 px-4 border-b">{job.title}</td>
                                 <td className="py-2 px-4 border-b max-sm:hidden">{moment(job.date).format("ll")}</td>
                                 <td className="py-2 px-4 border-b max-sm:hidden">{job.location}</td>
                                 <td className="py-2 px-4 border-b text-center">{job.applicants}</td>
                                 <td className="py-2 px-4 border-b">
-                                    <input onChange={() => changeJobVisibility(job._id)} className="scale-125 ml-4" type="checkbox" checked = {job.visible} />
+                                    <input onChange={() => changeJobVisibility(job._id)} className="scale-125 ml-4" type="checkbox" checked={job.visible} />
                                 </td>
                             </tr>
                         ))}
@@ -95,10 +100,10 @@ const changeJobVisibility = async (id) => {
                 </table>
             </div>
             <div className="mt-4 flex justify-end">
-                <button onClick={()=> navigate("/dashboard/add-job")} className="bg-black text-white py-2 px-4 rounded">Add new job</button>
+                <button onClick={() => navigate("/dashboard/add-job")} className="bg-black text-white py-2 px-4 rounded">Add new job</button>
             </div>
         </div>
-    )
+    ) : <Loading />
 }
 
 export default ManageJobs
--- client/src/pages/ViewApplications.jsx
+++ client/src/pages/ViewApplications.jsx
@@ -6,18 +6,18 @@ import { useState } from "react";
 import axios from "axios";
 import Loading from "../components/Loading";
 
-const ViewApplications  = () => {
+const ViewApplications = () => {
 
-    const {backendUrl, companyToken} = useContext(AppContext)
+    const { backendUrl, companyToken } = useContext(AppContext)
 
     const [applicants, setApplicants] = useState(false)
 
     // function to fetch company job applications data
     const fetchCompanyJobApplications = async () => {
         try {
-            
-            const {data} = await axios.get(backendUrl + "/api/company/applicants",
-                {headers: {token: companyToken}}
+
+            const { data } = await axios.get(backendUrl + "/api/company/applicants",
+                { headers: { token: companyToken } }
             )
 
             if (data.success) {
@@ -31,13 +31,37 @@ const ViewApplications  = () => {
         }
     }
 
+    // function to update job application status
+    const changeJobApplicationStatus = async (id, status) => {
+        try {
+
+            const { data } = await axios.post(backendUrl + "/api/company/change-status",
+                { id, status },
+                { headers: { token: companyToken } }
+            )
+
+            if (data.success) {
+                fetchCompanyJobApplications()
+            } else {
+                toast.error(data.message)
+            }
+
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
     useEffect(() => {
         if (companyToken) {
             fetchCompanyJobApplications()
         }
-    },[companyToken])
+    }, [companyToken])
 
-    return applicants ? applicants.lenght === 0 ? (<div></div>) : (
+    return applicants ? applicants.lenght === 0 ? (
+        <div className="flex items-center justify-center h-[70vh]">
+            <p className="text-xl sm:text-2xl">No Applications Avaiable</p>
+        </div>
+    ) : (
         <div className="container mx-auto p-4">
             <div>
                 <table className="w-full max-w-4xl bg-white border border-gray-200 max-sm:text-sm">
@@ -50,32 +74,36 @@ const ViewApplications  = () => {
                             <th className="py-2 px-4 text-left">Resume</th>
                             <th className="py-2 px-4 text-left">Action</th>
                         </tr>
-                    </thead> 
+                    </thead>
                     <tbody>
-                        {applicants.filter(item => item.jobId && item.userId).map((applicant, index)=> (
+                        {applicants.filter(item => item.jobId && item.userId).map((applicant, index) => (
                             <tr key={index} className="text-gray-700">
-                                <td className="py-2 px-4 border-b text-center">{index+1}</td>
-                                <td className="py-2 px-4 border-b text-center flex">
-                                    <img className="w-10 h-10 rounded-full mr-3 max-sm:hidden" src={applicant.imgSrc} alt=""/>
+                                <td className="py-2 px-4 border-b text-center">{index + 1}</td>
+                                <td className="py-2 px-4 border-b text-center flex items-center">
+                                    <img className="w-10 h-10 rounded-full mr-3 max-sm:hidden" src={applicant.userId.image} alt="" />
                                     <span>{applicant.userId.name}</span>
                                 </td>
                                 <td className="py-2 px-4 border-b max-sm:hidden">{applicant.jobId.title}</td>
                                 <td className="py-2 px-4 border-b max-sm:hidden">{applicant.jobId.location}</td>
                                 <td className="py-2 px-4 border-b">
-                                    <a href="" target="_blank"
-                                    className="bg-blue-50 text-blue-400 px-3 py-1 rounded inline-flex gap-2 items-center"
+                                    <a href={applicant.userId.resume} target="_blank"
+                                        className="bg-blue-50 text-blue-400 px-3 py-1 rounded inline-flex gap-2 items-center"
                                     >
                                         Resume <img src={assets.resume_download_icon} alt="" />
                                     </a>
                                 </td>
                                 <td className="py-2 px-4 border-b relative">
-                                    <div className="relative inline-block text-left group"> 
-                                        <button className="text-gray 500 action-button">...</button>
-                                        <div className="z-10 hidden absolute right-0 md:left-0 top-0 mt-2 w-32 bg-white border border-gray-200 rounded shadow group-hover:block">
-                                            <button className="block w-full text-left px-4 py-2 text-blue-500 hover:bg-gray-100">Accept</button>
-                                            <button className="block w-full text-left px-4 py-2 text-red-500 hover:bg-gray-100">Reject</button>
+                                    {applicant.status === "Pending"
+                                        ? <div className="relative inline-block text-left group">
+                                            <button className="text-gray 500 action-button">...</button>
+                                            <div className="z-10 hidden absolute right-0 md:left-0 top-0 mt-2 w-32 bg-white border border-gray-200 rounded shadow group-hover:block">
+                                                <button onClick={() => changeJobApplicationStatus(applicant._id, "Accepted")} className="block w-full text-left px-4 py-2 text-blue-500 hover:bg-gray-100">Accept</button>
+                                                <button onClick={() => changeJobApplicationStatus(applicant._id, "Rejected")} className="block w-full text-left px-4 py-2 text-red-500 hover:bg-gray-100">Reject</button>
+                                            </div>
                                         </div>
-                                    </div>
+                                        : <div>{applicant.status}</div>
+                                    }
+
                                 </td>
                             </tr>
                         ))}
