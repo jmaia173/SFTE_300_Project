191394c5e14e0191a66819e98f26bfd0f9f1c6b2
jmaia173 <jpmascarenhasmaia@gmail.com>
2025-11-25 16:40:32 -0800

    .

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,9 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",   // ⭐ REQUIRED for Render to avoid 404s
   plugins: [
     react(),
     tailwindcss(),
   ],
 })
-
