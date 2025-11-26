50f6d66207782620f4a54cfc5758f8c9fc223260
jmaia173 <jpmascarenhasmaia@gmail.com>
2025-11-25 16:01:22 -0800

    .

--- server/config/db.js
+++ server/config/db.js
@@ -1,13 +1,16 @@
-import mongoose from "mongoose"
-
-// Function to connect to the MongoDB database
+import mongoose from "mongoose";
 
 const connectDB = async () => {
+  mongoose.connection.on("connected", () => console.log("Database Connected"));
 
-    mongoose.connection.on("connected", () => console.log("Database Connected"))
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "job-portal", // <-- correct place to specify db
+    });
+  } catch (err) {
+    console.error("MongoDB Connection Error:", err);
+    process.exit(1);
+  }
+};
 
-    await mongoose.connect(`${process.env.MONGODB_URI}/job-portal`)
-
-}
-
-export default connectDB
+export default connectDB;
