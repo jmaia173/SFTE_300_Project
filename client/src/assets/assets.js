f3ee15f98e13c03bbbf32127cb9a80546533d201
jmaia173 <jpmascarenhasmaia@gmail.com>
2025-12-01 11:08:46 -0800

    .

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import search_icon from "./search_icon.svg";
 import company_icon from "./company_icon.svg";
 import microsoft_logo from "./microsoft_logo.svg";
-import walmart_logo from "./walmart_logo.png";
+import walmart_logo from "./walmart_logo.svg";
 import accenture_logo from "./accenture_logo.png";
 import profile_img from "./profile_img.png";
 import app_main_img from "./app_main_img.png";
